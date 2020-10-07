6d513d29d39ab8dd39c0131d2f8fa52b9e647b84
Nikhil-Swami <54004431+MasterMindNikhilSwami@users.noreply.github.com>
2020-10-07 22:18:40 +0530

    update 7-oct

--- modulex.py
+++ modulex.py
@@ -59,7 +59,7 @@ def make_soup(markup):
 	from bs4 import BeautifulSoup as soup
 	return soup(markup,'html5lib')
 
-def get_page(url,soupify=False):
+def get_page(url,soupify=True):
 	from bs4 import BeautifulSoup as soup
 	import html5lib,requests
 	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -85,12 +85,50 @@ def get_page_selenium(url,headless=True,strategy='normal'):
 def push_tab(client,url):
 	client.execute_script("window.open('{}', '_blank')".format(url))
 
-	
-                
 
+#______________________________________________
+#@#$%@#$%#$%#$%#+++CRYPTOGRAPHY+++#@$#@$#@$!@# 
+import random
+class Nikcipher:
+	def __init__(self, basepassword):
+		self.basepassword= basepassword
+		self.cryptSpace= self.makeCryptSpace()
+		self.viewCryptSpace= lambda :"".join(self.cryptSpace)
+		self.storage= self.cryptx()
+		self.get_keypass= self.make_key(self.storage[0],self.storage[1])
+	
+	def makeCryptSpace(self):
+		cryptSpace=  [chr(x) for x in range(ord("0"),ord("9")+1)]
+		cryptSpace+= [chr(x) for x in range(ord("a"),ord("z")+1)]
+		cryptSpace+= [chr(x) for x in range(ord("A"),ord("Z")+1)]
+		return cryptSpace
+
+	def cryptx(self,security=4):
+		def r(namespace,k=security):
+			return random.sample(namespace,k=k)
+
+		cryptSpace=self.cryptSpace
+		ordstr=[ord(x) for x in self.basepassword]
+		keySpace=[r(cryptSpace,k=1)[0] for x in range(len(self.basepassword)*security)]
+		keySpace="".join(keySpace)
+		return (self.basepassword,keySpace)
+
+	def make_key(self,stringx,keySpace):
+		ks_indices=random.sample(list(enumerate(keySpace)) ,k=len(stringx))
+		ord_add=[ord(s)+ord(p[1]) for s,p in zip(stringx,ks_indices)]
+		key=[str(ki[0])+'.'+str(oa) for ki,oa in zip(ks_indices,ord_add)]
+		key="+".join(key)
+		return key,keySpace
+
+	def decryptx(self,key,keyspace):
+		maps=key.split('+')
+		imods=[x.split('.') for x in maps]
+		originPass=[ chr(int(x[1]) - ord(keyspace[int(x[0])])) for x in imods ]
+		return originPass
 
 if __name__ == '__main__':
-	a="1+99"
-	# a=eval(a)
-	# print(a)
-
+	cryptengine=Nikcipher("nikhilswami1@gmail.com")
+	kp=cryptengine.get_keypass
+	fwrite('cryptEngineTest.txt',str(kp))
+	orignal=cryptengine.decryptx(kp[0],kp[1])
+	print('orignal password=={}'.format(orignal))

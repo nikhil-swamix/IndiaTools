a867137d285985bb6297ae102f9100f3413e3bee
Nikhil-Swami <54004431+BinarySwami-10@users.noreply.github.com>
2022-01-17 00:00:55 +0530

    modulex update

--- mxproxy.py
+++ mxproxy.py
@@ -1,28 +1,36 @@
 import sys
+import os
 
 
 def progressive_import():
-    success = 0
-    importlevel = "./"
-    try:
-        import modulex as mx
-    except Exception as e:
-        for i in range(4):
-            if not success:
-                try:
-                    sys.path.append(importlevel)
-                    from modulex import modulex as mx
-                    success = 1
-                    print("mx imported")
-                    mx.cleanup()
-                except Exception as e:
-                    ...
-        pass
+	global mx
+	success = 0
+	importlevel = "./"
+	try:
+		import modulex as mx
+		print("mx imported")
+	except Exception as e:
+		for i in range(4):
+			if not success:
+				try:
+					sys.path.append(importlevel)
+					from modulex import modulex as mx
+					success = 1
+					print("mx imported")
+					mx.cleanup()
+				except Exception as e:
+					...
+		pass
+
 
 def fetch_latest_copy():
-    progressive_import()
-    mx.get_page()
+	progressive_import()
+	# print((mx.get_page('https://raw.githubusercontent.com/BinarySwami-10/modulex/master/modulex.py').text))
+	mx.fwrite('modulexnew.py', mx.get_page(
+            'https://raw.githubusercontent.com/BinarySwami-10/modulex/master/modulex.py').text
+           )
 
-if __name__ != '__main__':
+
+fetch_latest_copy()
+if __name__ == '__main__':
 	fetch_latest_copy()
-
